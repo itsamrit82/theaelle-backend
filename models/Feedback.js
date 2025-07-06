686709a6d80d999206a91ed7663a5a5fb4fdf9b2
itsamrit82 <itsamrit82@gmail.com>
2025-07-06 10:47:52 +0530

    Delete models directory

--- models/Feedback.js
+++
@@ -1,9 +0,0 @@
-import mongoose from 'mongoose';
-
-const feedbackSchema = new mongoose.Schema({
-  user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-  product: { type: mongoose.Schema.Types.ObjectId, ref: 'Product', required: true },
-  comment: { type: String, required: true },
-}, { timestamps: true });
-
-export default mongoose.model('Feedback', feedbackSchema);
